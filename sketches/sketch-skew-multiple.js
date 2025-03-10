c8f881386ccb9043dc9017d82fff307774ffadb6
Bevan Sharp <bevan.sharp@gmail.com>
2025-03-10 19:35:35 +1100

    Fixed misaligned indentation

--- sketches/sketch-skew-multiple.js
+++ sketches/sketch-skew-multiple.js
@@ -17,9 +17,9 @@ const sketch = ({ context, width, height }) => {
   const nums = 40; //20;
   const rects = [];
   const rectColors = [
-	random.pick(risoColors),
-	random.pick(risoColors),
-	//random.pick(risoColors),
+    random.pick(risoColors),
+    random.pick(risoColors),
+    //random.pick(risoColors),
   ];
   const bgColor = random.pick(risoColors).hex;
 
@@ -32,18 +32,18 @@ const sketch = ({ context, width, height }) => {
     y = random.range(0, height);
     w = random.range(600, width); //random.range(200, 400);
     h = random.range(40, 200);
-	lineWidth = random.range(8, 24);
+    lineWidth = random.range(8, 24);
 
-	blendMode = random.value() > 0.5 ? 'overlay' : 'source-over';
+    blendMode = random.value() > 0.5 ? 'overlay' : 'source-over';
 
-	//fill = `rgba(${random.range(0, 255)}, ${random.range(0, 255)}, ${random.range(0, 255)})`;
-	//fill = random.pick(risoColors).hex;
-	fill = random.pick(rectColors).hex;
-	//stroke = 'black';
-	//stroke = random.pick(risoColors).hex;
-	stroke = random.pick(rectColors).hex;
-	
-	rects.push({ x, y, w, h, fill, stroke, lineWidth, blendMode });
+    //fill = `rgba(${random.range(0, 255)}, ${random.range(0, 255)}, ${random.range(0, 255)})`;
+    //fill = random.pick(risoColors).hex;
+    fill = random.pick(rectColors).hex;
+    //stroke = 'black';
+    //stroke = random.pick(risoColors).hex;
+    stroke = random.pick(rectColors).hex;
+
+    rects.push({ x, y, w, h, fill, stroke, lineWidth, blendMode });
   }
 
   return ({ context, width, height }) => {
@@ -51,57 +51,58 @@ const sketch = ({ context, width, height }) => {
     // Clear the canvas
     context.fillStyle = bgColor; context.fillRect(0, 0, width, height);
 
-	rects.forEach((rect) => {
-		const { x, y, w, h, fill, stroke, lineWidth, blendMode } = rect;
+    rects.forEach((rect) => {
+      const { x, y, w, h, fill, stroke, lineWidth, blendMode } = rect;
 
-		context.save();
+      context.save();
 
-		context.translate(x, y);
+      context.translate(x, y);
 
-		context.strokeStyle = stroke;
-		context.fillStyle = fill;
-		context.lineWidth = lineWidth;
+      context.strokeStyle = stroke;
+      context.fillStyle = fill;
+      context.lineWidth = lineWidth;
 
-		context.globalCompositeOperation = blendMode;
+      context.globalCompositeOperation = blendMode;
 
-		drawSkewedRect({ context, w, h, degrees });
-		
-		// Convert the fill color to a HSL value with a lower luminance (to make darker)
-		shadowColor = Color.offsetHSL(fill, 0, 0, -20);
-		shadowColor.rgba[3] = 0.5; // Set the alpha value to 50%
+      drawSkewedRect({ context, w, h, degrees });
 
-		context.shadowColor = Color.style(shadowColor.rgba);
-		context.shadowOffsetX = -10;
-		context.shadowOffsetY = 20;
-		context.fill();
+      // Convert the fill color to a HSL value with a lower luminance (to make darker)
+      shadowColor = Color.offsetHSL(fill, 0, 0, -20);
+      shadowColor.rgba[3] = 0.5; // Set the alpha value to 50%
 
-		context.shadowColor = null;
-		context.stroke();
+      context.shadowColor = Color.style(shadowColor.rgba);
+      context.shadowOffsetX = -10;
+      context.shadowOffsetY = 20;
+      context.fill();
 
-		context.globalCompositeOperation = 'source-over';
+      context.shadowColor = null;
+      context.stroke();
 
-		context.lineWidth = 2;
-		context.strokeStyle = 'black';
-		context.stroke();
+      context.globalCompositeOperation = 'source-over';
 
-		context.restore();
-	});
+      context.lineWidth = 2;
+      context.strokeStyle = 'black';
+      context.stroke();
+
+      context.restore();
+    });
 
   };
 };
 
 const drawSkewedRect = ({ context, w=600, h=200, degrees=45 }) => {
-	const angle = math.degToRad(degrees);
-	const rx = Math.cos(angle) * w;
-	const ry = Math.sin(angle) * w;
+  const angle = math.degToRad(degrees);
+  const rx = Math.cos(angle) * w;
+  const ry = Math.sin(angle) * w;
 
-	context.translate(rx * -0.5, (ry + h) * -0.5);
-	context.beginPath();
-	context.moveTo(0, 0);
-	context.lineTo(rx, ry);
-	context.lineTo(rx, ry + h);
-	context.lineTo(0, h);
-	context.closePath();
+  context.translate(rx * -0.5, (ry + h) * -0.5);
+  context.beginPath();
+  context.moveTo(0, 0);
+  context.lineTo(rx, ry);
+  context.lineTo(rx, ry + h);
+  context.lineTo(0, h);
+  context.closePath();
 };
 
 canvasSketch(sketch, settings);
+
