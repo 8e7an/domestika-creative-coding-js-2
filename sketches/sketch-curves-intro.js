c259b11b73d1494aafc5739745e181e992517a83
Bevan Sharp <bevan.sharp@gmail.com>
2025-03-13 17:22:09 +1100

    Add mouse event dragging functionality to the points of the curve

--- sketches/sketch-curves-intro.js
+++ sketches/sketch-curves-intro.js
@@ -1,16 +1,24 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 1024, 1024 ]
+  dimensions: [ 1080, 1080 ],
+  animate: true,
 };
 
-const sketch = () => {
+let canvasElement;
+let points;
 
-  const points = [
+const sketch = ({ canvas }) => {
+
+  points = [
     new Point({ x:200, y:540 }),
     new Point({ x:400, y:300, control:true }),
     new Point({ x:880, y:540 }),
   ];
+
+  canvas.addEventListener('mousedown', onMouseDown);
+  
+  canvasElement = canvas;
 
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
@@ -26,9 +34,51 @@ const sketch = () => {
   };
 };
 
+const onMouseDown = (evt) => {
+  const { x, y } = getCanvasCalculatedPosition(evt);
+
+  window.addEventListener('mousemove', onMouseMove);
+  window.addEventListener('mouseup', onMouseUp);
+
+  points.forEach((point) => {
+    point.isDragging = point.hitTest(x, y);
+  });
+};
+
+const onMouseMove = (evt) => {
+  const { x, y } = getCanvasCalculatedPosition(evt);
+
+  //console.log(evt.x, evt.y);
+  //console.log(evt.offsetX, evt.offsetY);
+  //console.log(x, y);
+
+  points.forEach((point) => {
+    if (point.isDragging) {
+      point.x = x;
+      point.y = y;
+    }
+  });
+};
+
+const onMouseUp = (evt) => {
+  window.removeEventListener('mousemove', onMouseMove);
+  window.removeEventListener('mouseup', onMouseUp);
+};
+
+const getCanvasCalculatedPosition = (evt) => {
+  //const x = (evt.offsetX / canvasElement.offsetWidth) * canvasElement.width;
+  //const y = (evt.offsetY / canvasElement.offsetHeight) * canvasElement.height;
+  return { 
+    x: (evt.offsetX / canvasElement.offsetWidth) * canvasElement.width,
+    y: (evt.offsetY / canvasElement.offsetHeight) * canvasElement.height
+  };
+};
+
 canvasSketch(sketch, settings);
 
 class Point {
+  static size = 10;
+  isDragging = false;
   constructor({ x, y, control = false }) {
     this.x = x;
     this.y = y;
@@ -39,8 +89,14 @@ class Point {
     context.translate(this.x, this.y);
     context.fillStyle = this.control ? 'red' : 'black';
     context.beginPath();
-    context.arc(0, 0, 10, 0, Math.PI * 2);
+    context.arc(0, 0, Point.size, 0, Math.PI * 2);
     context.fill()
     context.restore();
+  }
+  hitTest(x, y) {
+    const dx = this.x - x;
+    const dy = this.y - y;
+    const dd = Math.sqrt(dx**2 + dy**2);
+    return dd < Point.size * 2;
   }
 }
