630b4aef399173a54fff66d539cc4c335b36c476
Bevan Sharp <bevan.sharp@gmail.com>
2025-03-15 10:32:48 +1100

    [sketch-curves-animated.js] 2025.03.15-10.32.48

---
+++ sketches/sketch-curves-animated.js
@@ -0,0 +1,117 @@
+// https://www.domestika.org/en/courses/3862-creative-coding-2-0-in-js-animation-sound-color/units/14952-curves
+// sketch-curves-animated.js
+
+const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random');
+const math = require('canvas-sketch-util/math');
+const colormap = require('colormap');
+
+const settings = {
+  dimensions: [ 1080, 1080 ]
+};
+
+const sketch = ({ width, height }) => {
+  const drawPoints = false;
+  const randomColorMap = true;
+
+  const colorMaps = ['jet', 'hsv', 'cubehelix', 'bluered', 'winter',
+    'inferno', 'rainbow', 'rainbow-soft', 'magma', 'salinity', 'autumn', 'copper'];
+
+  const cols = 12;
+  const rows = 40;
+  const numCells = cols * rows;
+
+  // grid
+  const gw = width * 0.8;
+  const gh = height * 0.8;
+  // cells
+  const cw = gw / cols;
+  const ch = gh / rows;
+  // margins
+  const mx = (width - gw) * 0.5;
+  const my = (height - gh) * 0.5;
+
+  const points = [];
+
+  let x, y, n, lineWidth, color;
+  let frequency = 0.002;
+  let amplitude = 90;
+  let colors = colormap({
+    colormap: randomColorMap ? random.pick(colorMaps) : 'magma',
+      nshades: amplitude,
+  });
+
+  for (let i=0; i<numCells; i++) {
+    x = (i % cols) * cw;
+    y = Math.floor(i / cols) * ch;
+    n = random.noise2D(x, y, frequency, amplitude);
+    x += n;
+    y += n;
+    lineWidth = math.mapRange(n, -amplitude, amplitude, 2, 20);
+    color = colors[Math.floor(math.mapRange(n, -amplitude, amplitude, 0, amplitude))];
+    points.push(new Point({ x, y, lineWidth, color }));
+  }
+
+  return ({ context, width, height }) => {
+
+    // Clear the canvas to black
+    context.fillStyle = 'black'; context.fillRect(0, 0, width, height);
+
+    context.save();
+    context.translate(mx, my);
+    context.translate(cw * 0.5, ch * 0.5);
+    context.strokeStyle = 'orange';
+    context.lineWidth = 4;
+
+    let lastx, lasty;
+
+    //draw curves based on Point objects in the points array
+    for (let r=0; r<rows; r++) {
+      for (let c=0; c<cols-1; c++) {
+        const curr = points[r * cols + c + 0];
+        const next = points[r * cols + c + 1];
+        const mx = curr.x + (next.x - curr.x) * 0.8;
+        const my = curr.y + (next.y - curr.y) * 5.5;
+        if (!c) { lastx = curr.x; lasty = curr.y; }
+        context.beginPath();
+        context.lineWidth = curr.lineWidth;
+        context.strokeStyle = curr.color;
+        context.moveTo(lastx, lasty);
+        context.quadraticCurveTo(curr.x, curr.y, mx, my);
+        context.stroke();
+        lastx = mx - c / cols + 250; 
+        lasty = my - r / rows + 250;
+      }
+    }
+
+    //draw points
+    if (drawPoints) {
+      points.forEach((point) => {
+        point.draw(context);
+      });
+    }
+
+    context.restore();
+  };
+};
+
+canvasSketch(sketch, settings);
+
+class Point {
+  static size = 10;
+  constructor({ x, y, lineWidth, color }) {
+    this.x = x;
+    this.y = y;
+    this.lineWidth = lineWidth;
+    this.color = color;
+  }
+  draw(context) {
+    context.save();
+    context.translate(this.x, this.y);
+    context.fillStyle = 'red';
+    context.beginPath();
+    context.arc(0, 0, Point.size, 0, Math.PI * 2);
+    context.fill()
+    context.restore();
+  }
+}
